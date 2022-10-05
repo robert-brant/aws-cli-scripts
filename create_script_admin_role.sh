573fd4f44d51e09ca82ce5870e68faebad868b30
robert-brant <33581336+robert-brant@users.noreply.github.com>
2022-10-05 11:04:52 -0400

    Create create_script_admin_role.sh

---
+++ create_script_admin_role.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Create the scripts admin role and attach required policies
+AccountID=682591188398
+aws iam create-role --role-name script-admin-role --assume-role-policy-document file://aws_service_trust_policy.json
+aws iam create-policy --policy-name ec2-script-admin --policy-document file://policies/ec2_admin.json
+aws iam create-policy --policy-name iam-script-admin --policy-document file://policies/iam_admin.json
+aws iam attach-role-policy --policy-arn arn:aws:iam::$AccountID:policy/ec2-scripts-admin
+aws iam attach-role-policy --policy-arn arn:aws:iam::$AccountID:policy/iam-scripts-admin
