adaf4ef44ae4e9fcce38b2db335b34361d033142
robert-brant <33581336+robert-brant@users.noreply.github.com>
2022-10-06 18:37:15 -0400

    Update 01_create_admin_ec2.py

--- 01_create_admin_ec2.py
+++ 01_create_admin_ec2.py
@@ -1,13 +1,12 @@
 import json, os
 
 configFile = 'admin-ec2-config.json'
-securityGroupFile = 'script-admin-security-group-id.json'
+securityGroupFile = 'security-groups/script-admin-security-group-id.json'
 f = open(configFile)
 configData = json.load(f)
 f2 = open(securityGroupFile)
 securityData = json.load(f2)
 os.system('aws ec2 create-key-pair --key-name script-admin-key-pair > script-admin-key-pair.pem')
-os.system('aws ec2 run-instances --image-id '+ configData('ami-id')
-        + ' --count 1 --instance-type t2.micro --key-name script-admin-key-pair' +
-        + ' --security-group-ids ' + securityData('GroupId')
-        + ' --tag-specifications \'ResourceType=instance,Tags=[{Key=name,Value=script-admin}]\'')
+buildCommand = "aws ec2 run-instances --image-id {} --count 1 --instance-type t2.micro --key-name script-admin-key-pair --security-group-ids {} --tag-specifications 'ResourceType=instance,Tags=[{{Key=Name,Value=script-admin}}]' > admin-instance-details.json".format(configData['ami-id'],securityData['GroupId'])
+#print(buildCommand)
+os.system(buildCommand)
