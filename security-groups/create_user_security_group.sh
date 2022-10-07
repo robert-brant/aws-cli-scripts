5f2c79700676d50435c17d4aa62a7be1cbc0b6f2
robert-brant <33581336+robert-brant@users.noreply.github.com>
2022-10-07 13:04:02 -0400

    Delete create_user_security_group.sh

--- security-groups/create_user_security_group.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Creates the script user security group and attaches IP addresses which can use ssh to connect
-VPCID=vpc-a814cad0
-USERNAME=robertb
-aws ec2 create-security-group --group-name script-$USERNAME-security-group --description "Security group for pipeline resources" --vpc-id $VPCID > script-admin-security-group-id.json
-echo "Created script user security group."
