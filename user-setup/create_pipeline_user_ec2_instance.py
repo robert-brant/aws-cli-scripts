c7d83120728cab3884df73db556935bcff26da91
robert-brant <33581336+robert-brant@users.noreply.github.com>
2022-10-07 13:34:07 -0400

    Update create_pipeline_user_ec2_instance.py

--- user-setup/create_pipeline_user_ec2_instance.py
+++ user-setup/create_pipeline_user_ec2_instance.py
@@ -14,21 +14,29 @@ configData = json.load(f)
 # ssh -i script-user-key-pair.pem ec2-user@<PublicDnsName>
 # PublicDnsName should be in the user-instance-details.json or can be found using the instance id found in the user-instance-details.json with the following query:
 # aws ec2 describe-instances --instance-ids <instance id> --query 'Reservations[].Instances[].PublicDnsName'
-keyCommand = "aws ec2 create-key-pair --key-name script-{}-key-pair > script-{}-key-pair.pem".format(configData['iam-user'])
+keyCommand = "aws ec2 create-key-pair --key-name script-{}-key-pair > script-{}-key-pair.pem".format(configData['iam-user'],configData['iam-user'])
 # DEBUG print statement
 # print(keyCommand)
 # execute command
 os.system(keyCommand)
 
 ### CREATE SECURITY GROUP FOR THE EC2 INSTANCE ###
-securityCommand = "aws ec2 create-security-group --group-name script-{}-security-group --description 'Security group for administering build pipeline resources --vpc-id {} > script-{}-security-group-id.json".format(configData['iam-user'],configData['vpc-id'],configData['iam-user'])
+securityCommand = "aws ec2 create-security-group --group-name script-{}-security-group --description 'Security group for managing build pipeline resources' --vpc-id {} > script-{}-security-group-id.json".format(configData['iam-user'],configData['vpc-id'],configData['iam-user'])
 #DEBUG print statement
 # print(securityCommand)
 # execute command
 os.system(securityCommand)
 
-f2 = open("script-{}-security-group-id.json".format(configData['iam-user'])
+securityGroupIdFile = "script-{}-security-group-id.json".format(configData['iam-user'])
+f2 = open(securityGroupIdFile)
 securityData = json.load(f2)
+
+### GRANT PERMISSION TO CONNECT FROM IP ADDRESS ###
+ingressCommand = "aws ec2 authorize-security-group-ingress --group-id {} --protocol ssh --port 22 --cidr {}".format(securityData['GroupId'],configData['ip-address'])
+#DEBUG print statement
+# print(ingressCommand)
+# execute command
+os.system(ingressCommand)
 
 ### BUILD AND START THE EC2 INSTANCE ###
 buildCommand = "aws ec2 run-instances --image-id {} --count 1 --instance-type t2.micro --key-name script-{}-key-pair --security-group-ids {} --tag-specifications 'ResourceType=instance,Tags=[{{Key=Name,Value=script-user}}]' > user-instance-details.json".format(configData['ami-id'],configData['iam-user'],securityData['GroupId'])
